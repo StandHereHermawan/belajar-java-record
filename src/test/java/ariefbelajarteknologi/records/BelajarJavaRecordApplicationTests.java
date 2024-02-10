dc2ba6d3ea691a359ad8b6769f14d4e5824b3cd8
Arief-Karditya <nolifegansaya@gmail.com>
2024-02-10 16:32:13 +0700

    Create Project, Learn Java Record

---
+++ src/test/java/ariefbelajarteknologi/records/BelajarJavaRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajarteknologi.records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
